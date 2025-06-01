0234dfe7c8feee2f172a71b0cb50845c86fcfa01
dalermlbb <cassidymlbbbbb@gmail.com>
2025-06-01 11:28:42 +0500

    comit

---
+++ callback_10_misol/arr_methods.js
@@ -0,0 +1,46 @@
+// at(index)
+
+function elementniTop(array, index) {
+  let result
+}
+
+console.log(elementniTop(["a", "b", "c", "d"], -2)); // "c"
+
+
+// toString(), join()
+
+
+
+// push(), pop()
+
+
+
+// unshift(), shift()
+
+
+
+// reverse() vs toReversed()
+
+
+
+// splice(startIndex, countOfElementDeleted, elementAdded1, elementAdded2, …)
+
+
+
+// fill(value, startIndex, endIndex) [startIndex, endIndex)
+
+
+
+// slice(startIndex, endIndex) → new array [startIndex, endIndex)
+
+
+
+// includes(el) → boolean
+
+
+
+// indexOf(), lastIndexOf()
+
+
+
+// concat() → new array
